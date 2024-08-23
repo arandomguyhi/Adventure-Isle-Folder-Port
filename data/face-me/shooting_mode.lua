6dd9016e0aae0913a2d57bd6bd910762057c77f8
arandomguyhi <theorda52@gmail.com>
2024-08-23 15:02:14 -0300

    this is so bad help 😭

--- data/face-me/shooting_mode.lua
+++ data/face-me/shooting_mode.lua
@@ -5,7 +5,7 @@ local dogPath = 'characters/duck_season/'
 luaDebugMode = true
 function onCreate()
     for i = 1,2 do -- testing with 2 sprites for now
-        makeAnimatedLuaSprite('dogFront'..i, dogPath..'Dog_Shadow_Laugh', 0, 1350)
+        makeAnimatedLuaSprite('dogFront'..i, dogPath..'Dog_Shadow_Laugh', 1150, 1350)
         addAnimationByPrefix('dogFront'..i, 'idle', 'Dog Shadow Laugh instance 1', 24, true)
         addAnimationByPrefix('dogFront'..i, 'throw', 'Dog Laugh Shadow Throw instance 1', 24, false)
 	playAnim('dogFront'..i, 'idle')
@@ -25,7 +25,8 @@ function onCreate()
         makeAnimatedLuaSprite('dogRight'..i, dogPath..'Dog_Shadow')
         addAnimationByPrefix('dogRight'..i, 'idle', 'Dog shadow Idle instance 1', 24, true)
         addAnimationByPrefix('dogRight'..i, 'throw', 'Dog Shadow Throw instance 1', 24, false)
-        scaleObject('dogRight'..i, 1, 1)
+	playAnim('dogRight'..i, 'idle')
+        scaleObject('dogRight'..i, 2, 2)
 	setObjectOrder('dogRight'..i, getObjectOrder('bush'))
         setProperty('dogRight'..i..'.antialiasing', false)
         addLuaSprite('dogRight'..i)
@@ -45,11 +46,25 @@ function onEvent(name, value1, value2)
 	    doguin = dogSprGroup[randomIndex]
 
 	    if not getProperty(doguin..'.visible') then
-		if doguin == 'dogLeft1' or doguin == 'dogLeft2' then
-	    	    setProperty(doguin..'.x', getRandomInt(900,1400))
-	    	    setProperty(doguin..'.y', getRandomInt(1270,1310))
-		elseif doguin == 'dogFront1' or doguin == 'dogFront2' then
-		    setProperty('dogFront'..d..'.x', getRandomInt(1150, 2150))
+		-- THIS IS SO BAD HELÇP :SOB:
+		if doguin == 'dogLeft1' then
+	    	    setProperty('dogLeft1.x', (getProperty('dogLeft2.visible') and getX('dogLeft2')-getWidth('dogLeft2') or getRandomInt(900,1300)))
+	    	    setProperty('dogLeft1.y', getRandomInt(1270,1310))
+		elseif doguin == 'dogLeft2' then
+		    setProperty('dogLeft2.x', (getProperty('dogLeft1.visible') and getX('dogLeft1')+getWidth('dogLeft1') or getRandomInt(900,1300)))
+	    	    setProperty('dogLeft2.y', getRandomInt(1270,1310))
+		end
+		if doguin == 'dogFront1' then
+		    setProperty('dogFront1.x', (getProperty('dogFront2.visible') and getX('dogFront2')-getWidth('dogFront2') or getRandomInt(1150, 2150)))
+		elseif doguin == 'dogFront2' then
+		    setProperty('dogFront2.x', (getProperty('dogFront1.visible') and getX('dogFront1')+getWidth('dogFront1') or getRandomInt(1150, 2150)))
+		end
+		if doguin == 'dogRight1' then
+		    setProperty('dogRight1.x', (getProperty('dogRight2.visible') and getX('dogRight2')+getWidth('dogRight2')-5 or 2500))
+	    	    setProperty('dogRight1.y', getRandomInt(1200, 1250))
+		elseif doguin == 'dogRight2' then
+		    setProperty('dogRight2.x', (getProperty('dogRight1.visible') and getX('dogRight1')+getWidth('dogRight1')-10 or 2500))
+	    	    setProperty('dogRight2.y', getRandomInt(1200, 1250))
 	    	end
 
 		setProperty(doguin..'.visible', true)
@@ -96,4 +111,12 @@ function onUpdate(elapsed)
     else
 	setPropertyFromClass('flixel.FlxG', 'mouse.visible', false)
     end
+end
+
+function getWidth(obj)
+    return getProperty(obj..'.width')
+end
+
+function getX(obj)
+    return getProperty(obj..'.x')
 end
